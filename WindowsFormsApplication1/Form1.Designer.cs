ce2ea01ee993bc7f842bf91f1eed10f77399f9ef
cjc <1176583406@qq.com>
2020-10-24 16:02:33 +0800

    添加项目文件。

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace WindowsFormsApplication1 {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            DevExpress.XtraGrid.GridFormatRule gridFormatRule3 = new DevExpress.XtraGrid.GridFormatRule();
+            DevExpress.XtraEditors.FormatConditionRuleValue formatConditionRuleValue3 = new DevExpress.XtraEditors.FormatConditionRuleValue();
+            DevExpress.XtraGrid.GridFormatRule gridFormatRule4 = new DevExpress.XtraGrid.GridFormatRule();
+            DevExpress.XtraEditors.FormatConditionRuleValue formatConditionRuleValue4 = new DevExpress.XtraEditors.FormatConditionRuleValue();
+            this.gcUploadData = new DevExpress.XtraGrid.GridControl();
+            this.gvUploadData = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn4 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn5 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn6 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn7 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.repositoryItemCheckEdit2 = new DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit();
+            this.repositoryItemCheckedComboBoxEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemCheckedComboBoxEdit();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbIncrMoney = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbInitMoney = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbTotalYear = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gcUploadData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvUploadData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemCheckEdit2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemCheckedComboBoxEdit1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gcUploadData
+            // 
+            this.gcUploadData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gcUploadData.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gcUploadData.Location = new System.Drawing.Point(0, 69);
+            this.gcUploadData.MainView = this.gvUploadData;
+            this.gcUploadData.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gcUploadData.Name = "gcUploadData";
+            this.gcUploadData.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemCheckEdit2,
+            this.repositoryItemCheckedComboBoxEdit1});
+            this.gcUploadData.Size = new System.Drawing.Size(1073, 478);
+            this.gcUploadData.TabIndex = 2;
+            this.gcUploadData.Tag = "checkbox";
+            this.gcUploadData.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gvUploadData});
+            // 
+            // gvUploadData
+            // 
+            this.gvUploadData.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.gridColumn1,
+            this.gridColumn2,
+            this.gridColumn3,
+            this.gridColumn4,
+            this.gridColumn5,
+            this.gridColumn6,
+            this.gridColumn7});
+            gridFormatRule3.ApplyToRow = true;
+            gridFormatRule3.Name = "Format0";
+            formatConditionRuleValue3.Appearance.BackColor = System.Drawing.Color.Yellow;
+            formatConditionRuleValue3.Appearance.Options.UseBackColor = true;
+            formatConditionRuleValue3.Condition = DevExpress.XtraEditors.FormatCondition.Expression;
+            formatConditionRuleValue3.Expression = "([MonitorTemp] < [MonitorLspTemp] - [Temp_LSL] Or [MonitorTemp] > [MonitorLspTemp" +
+    "] + [Temp_USL]) And [IsMonitorRun] = True";
+            gridFormatRule3.Rule = formatConditionRuleValue3;
+            gridFormatRule4.ApplyToRow = true;
+            gridFormatRule4.Name = "Format1";
+            formatConditionRuleValue4.Appearance.BackColor = System.Drawing.Color.Red;
+            formatConditionRuleValue4.Appearance.ForeColor = System.Drawing.Color.White;
+            formatConditionRuleValue4.Appearance.Options.UseBackColor = true;
+            formatConditionRuleValue4.Appearance.Options.UseForeColor = true;
+            formatConditionRuleValue4.Condition = DevExpress.XtraEditors.FormatCondition.Equal;
+            formatConditionRuleValue4.Value1 = false;
+            gridFormatRule4.Rule = formatConditionRuleValue4;
+            this.gvUploadData.FormatRules.Add(gridFormatRule3);
+            this.gvUploadData.FormatRules.Add(gridFormatRule4);
+            this.gvUploadData.GridControl = this.gcUploadData;
+            this.gvUploadData.IndicatorWidth = 34;
+            this.gvUploadData.Name = "gvUploadData";
+            this.gvUploadData.OptionsCustomization.AllowSort = false;
+            this.gvUploadData.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.gvUploadData.OptionsSelection.EnableAppearanceFocusedRow = false;
+            this.gvUploadData.OptionsView.ColumnAutoWidth = false;
+            this.gvUploadData.OptionsView.ShowGroupPanel = false;
+            this.gvUploadData.OptionsView.ShowIndicator = false;
+            // 
+            // gridColumn1
+            // 
+            this.gridColumn1.Caption = "年份";
+            this.gridColumn1.FieldName = "year";
+            this.gridColumn1.Name = "gridColumn1";
+            this.gridColumn1.Visible = true;
+            this.gridColumn1.VisibleIndex = 0;
+            // 
+            // gridColumn2
+            // 
+            this.gridColumn2.Caption = "本金";
+            this.gridColumn2.FieldName = "money";
+            this.gridColumn2.Name = "gridColumn2";
+            this.gridColumn2.Visible = true;
+            this.gridColumn2.VisibleIndex = 1;
+            // 
+            // gridColumn3
+            // 
+            this.gridColumn3.Caption = "10%";
+            this.gridColumn3.FieldName = "p10";
+            this.gridColumn3.Name = "gridColumn3";
+            this.gridColumn3.Visible = true;
+            this.gridColumn3.VisibleIndex = 2;
+            // 
+            // gridColumn4
+            // 
+            this.gridColumn4.Caption = "15%";
+            this.gridColumn4.FieldName = "p15";
+            this.gridColumn4.Name = "gridColumn4";
+            this.gridColumn4.Visible = true;
+            this.gridColumn4.VisibleIndex = 3;
+            // 
+            // gridColumn5
+            // 
+            this.gridColumn5.Caption = "20%";
+            this.gridColumn5.FieldName = "p20";
+            this.gridColumn5.Name = "gridColumn5";
+            this.gridColumn5.Visible = true;
+            this.gridColumn5.VisibleIndex = 4;
+            // 
+            // gridColumn6
+            // 
+            this.gridColumn6.Caption = "25%";
+            this.gridColumn6.FieldName = "p25";
+            this.gridColumn6.Name = "gridColumn6";
+            this.gridColumn6.Visible = true;
+            this.gridColumn6.VisibleIndex = 5;
+            // 
+            // gridColumn7
+            // 
+            this.gridColumn7.Caption = "30%";
+            this.gridColumn7.FieldName = "p30";
+            this.gridColumn7.Name = "gridColumn7";
+            this.gridColumn7.Visible = true;
+            this.gridColumn7.VisibleIndex = 6;
+            // 
+            // repositoryItemCheckEdit2
+            // 
+            this.repositoryItemCheckEdit2.AutoHeight = false;
+            this.repositoryItemCheckEdit2.Name = "repositoryItemCheckEdit2";
+            // 
+            // repositoryItemCheckedComboBoxEdit1
+            // 
+            this.repositoryItemCheckedComboBoxEdit1.AutoHeight = false;
+            this.repositoryItemCheckedComboBoxEdit1.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repositoryItemCheckedComboBoxEdit1.Name = "repositoryItemCheckedComboBoxEdit1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.tbIncrMoney);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.tbInitMoney);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.tbTotalYear);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1073, 69);
+            this.panel1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(717, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 25);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "计算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(462, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "每年投入金额：";
+            // 
+            // tbIncrMoney
+            // 
+            this.tbIncrMoney.Location = new System.Drawing.Point(580, 22);
+            this.tbIncrMoney.Name = "tbIncrMoney";
+            this.tbIncrMoney.Size = new System.Drawing.Size(100, 25);
+            this.tbIncrMoney.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "初始金额：";
+            // 
+            // tbInitMoney
+            // 
+            this.tbInitMoney.Location = new System.Drawing.Point(331, 22);
+            this.tbInitMoney.Name = "tbInitMoney";
+            this.tbInitMoney.Size = new System.Drawing.Size(100, 25);
+            this.tbInitMoney.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "总年限：";
+            // 
+            // tbTotalYear
+            // 
+            this.tbTotalYear.Location = new System.Drawing.Point(103, 22);
+            this.tbTotalYear.Name = "tbTotalYear";
+            this.tbTotalYear.Size = new System.Drawing.Size(100, 25);
+            this.tbTotalYear.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1073, 547);
+            this.Controls.Add(this.gcUploadData);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gcUploadData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvUploadData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemCheckEdit2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemCheckedComboBoxEdit1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gcUploadData;
+        private DevExpress.XtraGrid.Views.Grid.GridView gvUploadData;
+        private DevExpress.XtraEditors.Repository.RepositoryItemCheckedComboBoxEdit repositoryItemCheckedComboBoxEdit1;
+        private DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit repositoryItemCheckEdit2;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn3;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn4;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn5;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn6;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn7;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbIncrMoney;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbInitMoney;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbTotalYear;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        
+        private int initMoney = 0;
+        private int incrMoney = 0;
+        private int len = 0;
+
+        private void Form1_Load(object sender, EventArgs e) {
+            
+        }
+
+        public class GridControlClass {
+            public int year { get; set; }
+            public int money { get; set; }
+            public string p10 { get; set; }
+            public string p15 { get; set; }
+            public string p20 { get; set; }
+            public string p25 { get; set; }
+            public string p30 { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            initMoney = string.IsNullOrEmpty(tbInitMoney.Text) ? 0 : Convert.ToInt32(tbInitMoney.Text);
+            incrMoney = string.IsNullOrEmpty(tbIncrMoney.Text) ? 0 : Convert.ToInt32(tbIncrMoney.Text);
+            len = string.IsNullOrEmpty(tbTotalYear.Text) ? 0 : Convert.ToInt32(tbTotalYear.Text) + 1;
+
+            gcUploadData.DataSource = new List<GridControlClass>();
+            var lstData = new List<GridControlClass>();
+
+            string lastP10 = string.Empty;
+            string lastP15 = string.Empty;
+            string lastP20 = string.Empty;
+            string lastP25 = string.Empty;
+            string lastP30 = string.Empty;
+            for (int i = 0; i < len; i++) {
+                var item = new GridControlClass {
+                    year = i,
+                    money = initMoney + (incrMoney * i)
+                };
+
+                if (i == 0) {
+                    lastP10 = item.p10 = item.money.ToString();
+                    lastP15 = item.p15 = item.money.ToString();
+                    lastP20 = item.p20 = item.money.ToString();
+                    lastP25 = item.p25 = item.money.ToString();
+                    lastP30 = item.p30 = item.money.ToString();
+                } else {
+                    lastP10 = (Convert.ToDecimal(lastP10) * Convert.ToDecimal((1 + 0.1)) + incrMoney).ToString();
+                    item.p10 = Convert.ToDecimal(lastP10).ToString("F2");
+
+                    lastP15 = (Convert.ToDecimal(lastP15) * Convert.ToDecimal((1 + 0.15)) + incrMoney).ToString();
+                    item.p15 = Convert.ToDecimal(lastP15).ToString("F2");
+
+                    lastP20 = (Convert.ToDecimal(lastP20) * Convert.ToDecimal((1 + 0.20)) + incrMoney).ToString();
+                    item.p20 = Convert.ToDecimal(lastP20).ToString("F2");
+
+                    lastP25 = (Convert.ToDecimal(lastP25) * Convert.ToDecimal((1 + 0.25)) + incrMoney).ToString();
+                    item.p25 = Convert.ToDecimal(lastP25).ToString("F2");
+
+                    lastP30 = (Convert.ToDecimal(lastP30) * Convert.ToDecimal((1 + 0.30)) + incrMoney).ToString();
+                    item.p30 = Convert.ToDecimal(lastP30).ToString("F2");
+                }
+
+                lstData.Add(item);
+            }
+
+
+
+            Monitor.Enter(gcUploadData.DataSource);
+            var lstLoading = gcUploadData.DataSource as List<GridControlClass>;
+            Monitor.Exit(gcUploadData.DataSource);
+
+            foreach (var item in lstData) {
+                lstLoading.Add(item);
+
+            }
+
+            gcUploadData.Invoke(new Action(() => {
+                gcUploadData.RefreshDataSource();
+            }));
+        }
+    }
+}
